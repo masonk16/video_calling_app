131d0af5908894f71e6c4f71ab48c3a74e5b255a
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-11-06 22:32:32 +0200

    Join component created and useEffect hook

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -13,13 +13,76 @@ function App() {
 
   // This useEffect hook will contain all event handlers
   // like participantJoined, participantLeft, etc.
-  useEffect(() => {}, []);
+  useEffect(() => {
+    meteredMeeting.on("remoteTrackStarted", (trackItem) => {
+      remoteTracks.push(trackItem);
+      setRemoteTracks([...remoteTracks]);
+    });
+
+    meteredMeeting.on("remoteTrackStopped", (trackItem) => {
+      for (let i = 0; i < remoteTracks.length; i++) {
+        if (trackItem.streamId === remoteTracks[i].streamId) {
+          remoteTracks.splice(i, 1);
+        }
+      }
+      setRemoteTracks([...remoteTracks]);
+    });
+
+    meteredMeeting.on("participantJoined", (localTrackItem) => {});
+
+    meteredMeeting.on("participantLeft", (localTrackItem) => {});
+
+    meteredMeeting.on("onlineParticipants", (onlineParticipants) => {});
+
+    return () => {
+      meteredMeeting.removeListener("remoteTrackStarted");
+      meteredMeeting.removeListener("remoteTrackStopped");
+      meteredMeeting.removeListener("participantJoined");
+      meteredMeeting.removeListener("participantLeft");
+      meteredMeeting.removeListener("onlineParticipants");
+    };
+  });
 
   // Calls the API to create a new room and join the user.
-  function handleCreateMeeting(username) {}
+  async function handleCreateMeeting(username) {
+    // Call create room API
+    const { data } = await axios.post(API_LOCATION + "/api/create/room");
+    // Get metered domain and roomName from response
+    const METERED_DOMAIN = response.data.METERED_DOMAIN;
+    const roomName = data.roomName;
+
+    // Call join() method of the SDK
+    const joinResponse = await meteredMeeting.join({
+      name: username,
+      roomURL: METERED_DOMAIN + "/" + roomName,
+    });
+
+    //Update meetingJoined state to true
+    setMeetingJoined(true);
+  }
 
   // Calls API to validate the room and join the user
-  function handleJoinMeeting(roomName, username) {}
+  async function handleJoinMeeting(roomName, username) {
+    // Call API to validate the roomName
+    const response = await axios.get(API_LOCATION + "/api/validate-meeting?roomName=" + roomName);
+
+    if (response.data.roomFound) {
+      // Call API to fetch Metered Domain
+      const { data } = await axios.get(API_LOCATION + "/api/metered-domain");
+
+      // Extract Metered Domain and roomName from response
+      const METERED_DOMAIN = data.METERED_DOMAIN;
+
+      // Call join() method of the SDK
+      const joinResponse = await meteredMeeting.join({
+        name: username,
+        roomURL: METERED_DOMAIN + "/" + roomName,
+      });
+      setMeetingJoined(true);
+    } else {
+      alert("Invalid roomName");
+    }
+  }
 
   return (
     <div className="App">
---
+++ react-frontend/src/Join.js
@@ -0,0 +1,75 @@
+import useState from "react";
+
+function Join({ handleCreateMeeting, handleJoinMeeting }) {
+    const [username, setUsername] = useState("");
+    const [roomName, setRoomName] = useState("");
+
+    return (
+        <div id="joinView" className="w-full items-center justify-center flex">
+          <div className="bg-base-300 w-11/12 max-w-screen-md  rounded mt-48 p-10">
+            <div>
+              <label className="label">
+                <span className="label-text">Name:</span>
+              </label>
+              <input
+                value={username}
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
+                type="text"
+                className="w-full input input-primary input-bordered"
+                placeholder="Enter your name"
+              />
+            </div>
+    
+            <div className="divider">AND</div>
+    
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Meeting ID</span>
+             </label>
+          <div className="relative">
+            <input
+              value={roomName}
+              onChange={(e) => {
+                setRoomName(e.target.value);
+              }}
+              id="meetingId"
+              type="text"
+              placeholder="Meeting ID"
+              className="w-full pr-16 input input-primary input-bordered"
+            />
+            <button
+              id="joinExistingMeeting"
+              className="absolute top-0 right-0 rounded-l-none btn btn-primary text-xs"
+            >
+              <span
+                onClick={() => {
+                  handleJoinMeeting(roomName, username);
+                }}
+                className="hidden sm:block"
+              >
+                Join Existing Meeting
+              </span>
+              <span className="sm:hidden">Join</span>
+              </button>
+          </div>
+        </div>
+        <div className="divider">OR</div>
+        <div className="flex justify-center">
+          <button
+            onClick={() => {
+              handleCreateMeeting(username);
+            }}
+            id="createANewMeeting"
+            className="btn btn-primary"
+          >
+            Create a new meeting
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Join;
