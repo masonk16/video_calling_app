434eb910e81cb4fda7e829f4bcbd5f5c1eb34e82
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-11-06 21:43:21 +0200

    Metered SDK initialization

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Join from "./Join";
+import Meeting from "./Meeting";
+
+// Initializing Metered Video SDK
+const meteredMeeting = new window.Metered.Meeting();
 
 function App() {
+  // Will be set to true when user joins the meeting and update the UI
+  const [meetingJoined, setMeetingJoined] = useState(false);
+  // Storing onlineUsers, updating when a user joins or leaves the meeting
+  const [onlineUsers, setOnlineUsers] = useState([]);
+
+  // This useEffect hook will contain all event handlers
+  // like participantJoined, participantLeft, etc.
+  useEffect(() => {}, []);
+
+  // Calls the API to create a new room and join the user.
+  function handleCreateMeeting(username) {}
+
+  // Calls API to validate the room and join the user
+  function handleJoinMeeting(roomName, username) {}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {meetingJoined ? (
+        <Meeting onlineUsers={onlineUsers}/>
+      ) : (
+        <Join 
+          handleCreateMeeting={handleCreateMeeting}
+          handleJoinMeeting={handleJoinMeeting}
+        />
+      )}
     </div>
   );
+
 }
 
 export default App;
